65dba9cd7b476416181956e7c5f0c4c9e702d939
yogesh2104 <singhyogesh2104@gmail.com>
2024-01-14 23:41:04 +0530

    re verify vercel json file

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
-echo "BUILD START"
-python3.10 -m pip install -r requirements.txt
-python3.10 manage.py runserver
-echo "BUILD END"
+ echo "BUILD START"
+ python3.9 -m pip install -r requirements.txt
+ python3.9 manage.py runserver --noinput --clear
+ echo "BUILD END"
